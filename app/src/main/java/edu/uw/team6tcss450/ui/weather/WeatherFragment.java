4a19176c8f3b45011d17a20679008e1a801dce56
Andrew Lim <2000andrewlim@gmail.com>
2021-05-17 20:19:22 -0700

    you can check the weather now lol

--- app/src/main/java/edu/uw/team6tcss450/ui/weather/WeatherFragment.java
+++ app/src/main/java/edu/uw/team6tcss450/ui/weather/WeatherFragment.java
@@ -1,50 +1,110 @@
 package edu.uw.team6tcss450.ui.weather;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
 
 import edu.uw.team6tcss450.R;
+import edu.uw.team6tcss450.databinding.FragmentSignInBinding;
+import edu.uw.team6tcss450.databinding.FragmentWeatherBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class WeatherFragment extends Fragment {
+    private FragmentWeatherBinding binding;
 
-    List<WeatherModel> weatherList;
+    private final String url = "https://api.openweathermap.org/data/2.5/weather";
+    private final String appid = "3d3d39ca7103cb14be20aa0681bc291d";
+    DecimalFormat df = new DecimalFormat("#.##");
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        fill();
-
-        View view = inflater.inflate(R.layout.fragment_weather, container, false);
-        RecyclerView mRecyclerView = (RecyclerView) view.findViewById(R.id.RecyclerView_Weather);
-
-        WeatherRecyclerViewAdapter listAdapter = new WeatherRecyclerViewAdapter(weatherList);
-        mRecyclerView.setAdapter(listAdapter);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        mRecyclerView.setLayoutManager(layoutManager);
-
-        return view;
+        binding = FragmentWeatherBinding.inflate(inflater);
+        return binding.getRoot();
     }
 
-    private void fill() {
-        weatherList = new ArrayList<>();
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.buttonCity.setOnClickListener(this::getWeatherDetails);
+    }
 
-        weatherList.add(new WeatherModel("Wednesday", R.drawable.ic_cloud_black_24dp, "69", "48"));
-        weatherList.add(new WeatherModel("Thursday", R.drawable.ic_cloud_black_24dp, "65", "47"));
-        weatherList.add(new WeatherModel("Friday", R.drawable.ic_baseline_wb_sunny_24, "72", "50"));
-        weatherList.add(new WeatherModel("Saturday", R.drawable.ic_baseline_wb_sunny_24, "73", "51"));
-        weatherList.add(new WeatherModel("Sunday", R.drawable.ic_cloud_black_24dp, "67", "45"));
+    public void getWeatherDetails(View view) {
+        String tempurl = "";
+        String city = binding.editTextSearchbar.getText().toString();
+
+        if (city.equals("")) {
+            binding.textViewOutput.setText("City field cannot be empty!");
+        } else {
+            tempurl = url + "?q=" + city + "&appid=" + appid + "&units=imperial";
+
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, tempurl, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+                        JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                        String description = jsonObjectWeather.getString("description");
+                        JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                        double temp = jsonObjectMain.getDouble("temp");
+                        double feelsLike = jsonObjectMain.getDouble("feels_like");
+                        float pressure = jsonObjectMain.getInt("pressure");
+                        int humidity = jsonObjectMain.getInt("humidity");
+                        JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                        String wind = jsonObjectWind.getString("speed");
+                        JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
+                        String clouds = jsonObjectClouds.getString("all");
+                        JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
+                        String countryName = jsonObjectSys.getString("country");
+                        String cityName = jsonResponse.getString("name");
+                        String output = "Current weather of " + cityName + " (" + countryName + ")"
+                                + "\nTemp: " + df.format(temp) + " °F"
+                                + "\nFeels Like: " + df.format(feelsLike) + " °F"
+                                + "\nHumidity: " + humidity + "%"
+                                + "\nDescription: " + description
+                                + "\nWind Speed: " + wind + "m/s"
+                                + "\nCloudiness: " + clouds + "%"
+                                + "\nPressure: " + pressure + "hPa";
+                        binding.textViewOutput.setText(output);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    binding.textViewOutput.setText(error.toString());
+                }
+            });
+            RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+            queue.add(stringRequest);
+        }
     }
 }
