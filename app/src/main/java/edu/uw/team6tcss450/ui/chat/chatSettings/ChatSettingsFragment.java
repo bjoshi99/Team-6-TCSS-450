a0cfce49d04c0035ac90189de17ca8db0a905339
Alexis <al.orellano23@gmail.com>
2021-06-03 14:56:02 -0700

    Chat Settings updated to include real data in drop down menu

--- app/src/main/java/edu/uw/team6tcss450/ui/chat/chatSettings/ChatSettingsFragment.java
+++ app/src/main/java/edu/uw/team6tcss450/ui/chat/chatSettings/ChatSettingsFragment.java
@@ -6,20 +6,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.SpinnerAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import edu.uw.team6tcss450.R;
 import edu.uw.team6tcss450.databinding.FragmentChatListBinding;
 import edu.uw.team6tcss450.databinding.FragmentChatSettingsBinding;
+import edu.uw.team6tcss450.model.UserInfoViewModel;
 import edu.uw.team6tcss450.ui.chat.ChatFragmentArgs;
+import edu.uw.team6tcss450.ui.chat.chatList.ChatListFragmentDirections;
+import edu.uw.team6tcss450.ui.chat.chatList.ChatListViewModel;
+import edu.uw.team6tcss450.ui.contact.Contact;
+import edu.uw.team6tcss450.ui.contact.ContactModel;
 
 public class ChatSettingsFragment extends Fragment {
+    private ContactModel mModel;
+    private UserInfoViewModel mUserModel;
 
     public ChatSettingsFragment(){
         //Required empty public constructor.
@@ -32,6 +44,17 @@ public class ChatSettingsFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ViewModelProvider provider = new ViewModelProvider(getActivity());
+
+        mUserModel = provider.get(UserInfoViewModel.class);
+        mModel = provider.get(ContactModel.class);
+        mModel.connectGet(mUserModel.getmJwt());
+    }
+
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
@@ -39,16 +62,20 @@ public class ChatSettingsFragment extends Fragment {
         ChatSettingsFragmentArgs args = ChatSettingsFragmentArgs.fromBundle(getArguments());
         binding.chatRoomName.setText(args.getChatName());
 
-        ArrayList<String> contacts = new ArrayList<>();
+        mModel.addContactListObserver(getViewLifecycleOwner(), contactList -> {
+            System.out.println(contactList);
 
-        for (int i = 0; i < 10; i++) {
-            contacts.add(new String("Fake User " + i));
-        }
+            ArrayList<String> contacts = new ArrayList<>();
 
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<String>(getActivity().getApplicationContext(),  android.R.layout.simple_spinner_dropdown_item, contacts);
-        adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item);
+            for (int i = 0; i < contactList.size(); i++) {
+                contacts.add(contactList.get(i).getEmail());
+            }
 
-        binding.spinner.setAdapter(adapter);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),  android.R.layout.simple_spinner_dropdown_item, contacts);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            binding.spinner.setAdapter(adapter);
+
+        });
     }
 }
