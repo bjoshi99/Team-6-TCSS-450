2e9a2001e0f7c8f5ae6f5bf0879183351cd9e5bd
Alexis <al.orellano23@gmail.com>
2021-06-03 16:27:58 -0700

    users can be added to a groupchat

--- app/src/main/java/edu/uw/team6tcss450/ui/chat/chatSettings/ChatSettingsFragment.java
+++ app/src/main/java/edu/uw/team6tcss450/ui/chat/chatSettings/ChatSettingsFragment.java
@@ -2,6 +2,7 @@ package edu.uw.team6tcss450.ui.chat.chatSettings;
 
 import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,16 +17,32 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.IntFunction;
 
 import edu.uw.team6tcss450.R;
 import edu.uw.team6tcss450.databinding.FragmentChatListBinding;
 import edu.uw.team6tcss450.databinding.FragmentChatSettingsBinding;
+import edu.uw.team6tcss450.io.RequestQueueSingleton;
 import edu.uw.team6tcss450.model.UserInfoViewModel;
 import edu.uw.team6tcss450.ui.chat.ChatFragmentArgs;
 import edu.uw.team6tcss450.ui.chat.chatList.ChatListFragmentDirections;
 import edu.uw.team6tcss450.ui.chat.chatList.ChatListViewModel;
+import edu.uw.team6tcss450.ui.chat.chatList.ChatRoom;
 import edu.uw.team6tcss450.ui.contact.Contact;
 import edu.uw.team6tcss450.ui.contact.ContactModel;
 
@@ -76,6 +93,56 @@ public class ChatSettingsFragment extends Fragment {
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             binding.spinner.setAdapter(adapter);
 
+            binding.addButton.setOnClickListener(button -> {
+                int memberId = contactList.get(binding.spinner.getSelectedItemPosition()).memberId;
+
+                System.out.println(memberId);
+
+                String url = getActivity().getApplication().getResources().getString(R.string.base_url) +
+                        "chats/user/" + args.getChatID() + "/" +memberId;
+
+
+
+                Request request = new JsonObjectRequest(
+                        Request.Method.PUT,
+                        url,
+                        null, //push token found in the JSONObject body
+                        null, // we get a response but do nothing with it
+                        this::handleError) {
+
+                    @Override
+                    public Map<String, String> getHeaders() {
+                        Map<String, String> headers = new HashMap<>();
+                        // add headers <key,value>
+                        headers.put("Authorization", mUserModel.getmJwt());
+                        return headers;
+                    }
+                };
+
+                request.setRetryPolicy(new DefaultRetryPolicy(
+                        10_000,
+                        DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                        DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                //Instantiate the RequestQueue and add the request to the queue
+                RequestQueueSingleton.getInstance(getActivity().getApplication().getApplicationContext())
+                        .addToRequestQueue(request);
+            });
+
         });
+    }
+
+
+
+    private void handleError(final VolleyError error) {
+        if (Objects.isNull(error.networkResponse)) {
+            Log.e("NETWORK ERROR", error.getMessage());
+        }
+        else {
+            String data = new String(error.networkResponse.data, Charset.defaultCharset());
+            Log.e("CLIENT ERROR",
+                    error.networkResponse.statusCode +
+                            " " +
+                            data);
+        }
     }
 }
