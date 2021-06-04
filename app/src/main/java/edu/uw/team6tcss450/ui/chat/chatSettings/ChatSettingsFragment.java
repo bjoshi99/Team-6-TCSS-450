86d8e5b5bf19f2aec4eed0b4df33c1768ff8b9a2
Alexis <al.orellano23@gmail.com>
2021-06-03 17:32:37 -0700

    you can now remove other users from chat

--- app/src/main/java/edu/uw/team6tcss450/ui/chat/chatSettings/ChatSettingsFragment.java
+++ app/src/main/java/edu/uw/team6tcss450/ui/chat/chatSettings/ChatSettingsFragment.java
@@ -143,6 +143,41 @@ public class ChatSettingsFragment extends Fragment {
                         .addToRequestQueue(request);
             });
 
+            binding.removeButton.setOnClickListener(button -> {
+                String email = contactList.get(binding.spinner.getSelectedItemPosition()).getEmail();
+
+                System.out.println(email);
+
+                String url = getActivity().getApplication().getResources().getString(R.string.base_url) +
+                        "chats/" + args.getChatID() + "/" +email;
+
+
+
+                Request request = new JsonObjectRequest(
+                        Request.Method.DELETE,
+                        url,
+                        null, //push token found in the JSONObject body
+                        null, // we get a response but do nothing with it
+                        this::handleError) {
+
+                    @Override
+                    public Map<String, String> getHeaders() {
+                        Map<String, String> headers = new HashMap<>();
+                        // add headers <key,value>
+                        headers.put("Authorization", mUserModel.getmJwt());
+                        return headers;
+                    }
+                };
+
+                request.setRetryPolicy(new DefaultRetryPolicy(
+                        10_000,
+                        DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                        DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                //Instantiate the RequestQueue and add the request to the queue
+                RequestQueueSingleton.getInstance(getActivity().getApplication().getApplicationContext())
+                        .addToRequestQueue(request);
+            });
+
         });
 
     }
